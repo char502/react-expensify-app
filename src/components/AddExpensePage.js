098dddc38fe8c8cae46d5e1e979df33bf1c0bc52
char502 <char502@hotmail.com>
2018-10-14 15:47:48 +0100

    test cases for filters action

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { connect } from 'react-redux'
-import ExpenseForm from './ExpenseForm';
-import { addExpense } from '../actions/expenses';
+import React from "react";
+import { connect } from "react-redux";
+import ExpenseForm from "./ExpenseForm";
+import { addExpense } from "../actions/expenses";
 
-
-const AddExpensePage = (props) => (
-    <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm
-            onSubmit={(expense) => {
-                console.log(expense); // calling a prop - pulling the data through?
-                props.dispatch(addExpense(expense));
-                props.history.push('/'); // available as rendering component within react router
-            }}
-        />
-    </div>
+const AddExpensePage = props => (
+  <div>
+    <h1>Add Expense</h1>
+    <ExpenseForm
+      onSubmit={expense => {
+        console.log(expense); // calling a prop - pulling the data through?
+        props.dispatch(addExpense(expense));
+        props.history.push("/"); // available as rendering component within react router
+      }}
+    />
+  </div>
 );
 
 export default connect()(AddExpensePage);
