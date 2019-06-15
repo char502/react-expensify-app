5bf4285c3079091996dd5d818986bfa586463d76
char502 <char502@hotmail.com>
2019-06-15 16:04:26 +0100

    tweak to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,58 @@
-
 // core files need to provide, the input (entry) and the output
-
 
 // module.exports is a node command, enabling you to expose something (in this case, an object) to another file
 
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = (env) => {
-    const isProduction = env === 'production';
-    const CSSExtract = new ExtractTextPlugin('styles.css');
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
 
-    return {
-        // mode: 'development',
-        entry: './src/app.js',
-        output: {
-            path: path.join(__dirname, 'public', 'dist'),
-            filename: 'bundle.js'
+  return {
+    // mode: 'development',
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public", "dist"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
         },
-        module: {
-            rules: [{
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            }, {
-                test: /\.s?css$/,
-                use: CSSExtract.extract({
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        }
-
-                    ]
-                })
-            }]
-        },
-        plugins: [
-            CSSExtract
-        ],
-        devtool: isProduction ? 'source-map' : 'inline-source-map',
-        devServer: {
-            contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true,
-            publicPath: '/dist/'
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
         }
-    };
+      ]
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+      publicPath: "/dist/"
+    }
+  };
 };
-
 
 // A loader lets you to customise the behaviour of webpack when it loads a given file i.e. every time see a js file do this (also have loaders i.e. for scss converted down to css etc)
 
